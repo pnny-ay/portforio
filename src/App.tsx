6a279ce26c013b60e23e65ba71ed99372e19f76c
akari yabana <subaru.barisaku@gmail.com>
2021-01-20 23:27:52 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 300,
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="src/shell2_resize.jpg"
+          title="hana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            test
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
-
-export default App;
---
+++ src/component/tileData.tsx
@@ -0,0 +1,16 @@
+const tileData = [
+    {
+        img: './image/hana.jpg',
+        title: 'hana',
+        author: 'hanaya',
+        cols: 1,
+    },
+    {
+        img: './image/hana2.jpg',
+        title: 'hana2',
+        author: 'hanaya',
+        cols: 2,
+    },
+]
+
+export default tileData;
